a94cab688eee1eb729da83c17967609ddaa3c032
manojvmanduva <manojvmanduva@gmail.com>
2019-07-23 21:58:30 +0200

    Add files via upload

---
+++ graphics.py
@@ -0,0 +1,70 @@
+from turtle import*
+import random
+
+
+screensize(1500,1500)
+dot(1,"red")
+bgcolor("white")
+colormode(255)
+speed(10)
+
+
+
+hideturtle()
+t = textinput("Text to Display","abc")
+s = textinput("Text to Graphics","Enter your sentence")
+
+
+pensize(1)
+
+w = s.split()
+y = 0;
+
+
+while y < len(w):
+    word = w[y]
+    print (y)
+    print (word)
+    y =y+1;
+    r=random.randint(0,254);
+    g=random.randint(0,254);
+    b=random.randint(0,254);
+    pencolor(r,g,b)
+    fillcolor(r,g,b)
+    forward(200)
+    x = 0;
+
+    begin_fill()
+    while x < len(word):
+        alpha = word[x]
+        print (x)
+        print (alpha)
+        x = x+1;
+
+        alphabet = ["0","a","b","c","d","e","f","g","h","i","j","k","l","n","m","o","p","q","r","s","t","u","v","w","x","y","z"]
+        p = alphabet.index(alpha)
+        print (p)
+        circle (p*5)
+        right(45)
+    end_fill()
+
+penup()
+if len(w)>3:
+    setpos(-(len(w)*50),0)
+else :
+    setpos(0,0)
+pendown()
+pencolor("black")
+write(t, font=("Helvetica", 28, "normal"))
+    
+        
+
+
+
+
+
+
+
+
+
+
